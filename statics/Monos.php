bff785c5ceb6a1dfa73e8812faf8c763d430e978
ConnorSouls <pman39868@gmail.com>
2023-05-16 16:11:41 -0600

    Avance 2

--- statics/Monos.php
+++ statics/Monos.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividad MONO</title>
+    <link rel="stylesheet" href="./Style/MonosCool.css">
 </head>
 <body>
     <?php 
+              
         $palabra = array(
             0 => "lámpara",
             1 => "avión",
@@ -15,7 +17,7 @@
             3 => "pelota",
             4 => "sonrisa",
             5 => "árbol",
-            6=> "montaña",
+            6 => "montaña",
             7 => "taza",
             8 => "libro",
             9 => "mariposa",
@@ -58,33 +60,74 @@
             46 => "carro",
             47 => "cerrojo",
             48 => "lámpara",
-            49 => "ratón"
+            49 => "ratón",
+            50 => "patata",
         );
-        $numerito = rand (0,49);
+        
+        $cadFrase = array(
+            0 => "En",
+            1 => "medio",
+            2 => "del",
+            3 => "caos",
+            4 => "y",
+            5 => "la",
+            6 => "destrucción",
+            7 => "el",
+            8 => "Doom",
+            9 => "Slayer",
+            10 => "emerge",
+            11 => "como",
+            12 => "un",
+            13 => "símbolo",
+            14 => "de",
+            15 => "pesperanza"
+        );
+
+        function frase($cadFrase)
+        {
+            for($num=0; $num <= 15; $num++)
+            {
+                echo "<style> background-color: blue </style>";
+                echo $cadFrase[$num]." ";
+            }
+        }  
         
     ?>
 
-
-
-<table align="center" border="2" style="border-collapse: collapse;" cellpadding=30px>
+    <table align="center" border="2" style="border-collapse: collapse;" cellpadding=30px>
         <thead>
             <tr>
                 <th>
-                    Libro de Monito: <?php echo $palabra[$numerito];?>
+                    Libro de Monito: 
+                    <?php 
+                        $numpalabrastitulo = rand (1,5);
+                        for ($i=1; $i <= $numpalabrastitulo ; $i++) 
+                        { 
+                            $numerito = rand (0,49);
+                            echo $palabra[$numerito]." ";
+                        }    
+                    ?>
                 </th>
             </tr>
         </thead>
+
         <tbody>
             <tr>
                 <td>
                     <?php 
-                        for ($c=1; $c<=10; $c++)
+                        $comprobador = 0;
+                        for ($c=1; $c<=300; $c++)
                         {
-                            for ($c2=1; $c2 <=15 ; $c2++) { 
-                                $numerito = rand (0,49);
-                                echo $palabra[$numerito]." ";
+                            $numerito = rand (0,50);
+                            if ($numerito == 50 && $comprobador==0)
+                            {
+                                frase($cadFrase);
+                                $comprobador ++;
                             }
-                            echo "<br>";
+                            else{
+                                echo $palabra[$numerito]." ";
+                                
+                            }
                         }
                         
                     ?>
